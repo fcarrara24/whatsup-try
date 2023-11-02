741f8bf63a5e8a83036b2912d6567c3af936392a
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-11-02 13:05:31 +0100

    created chosen contact dimensions in sidebar

--- js/script.js
+++ js/script.js
@@ -18,16 +18,24 @@ const obj = {
     methods: {
 
         detectSend() {
-            this.messageList[this.index] = this.messageToSend;
-            let msgContainer = `<div class="d-flex justify-content-${personIsMe} pb-1">
-            <div class="card p-1 message-content bg-warning">
-                ${this.messageList[this.index]}
-            </div>
-            </div>`;
-            this.index = this.index + 1;
+            if (this.messageToSend !== '') {
+                //changing code to secure it
+                let savedMsg = this.messageToSend.replace(/</g, '<i class="fa-solid fa-less-than" style="font-size: 0.7em!important; padding-top: 7px;"></i>');
+                //stored all messages for future updates
+                this.messageList[this.index] = savedMsg;
 
-            this.totalMsg += msgContainer;
-            this.messageToSend = ''
+                let msgContainer = `
+                <div class="d-flex justify-content-${personIsMe} pb-1">
+                    <div class="card p-1 message-content bg-${personIsMe} d-flex flex-row">
+                        ${this.messageList[this.index]}
+                    </div>
+                </div>`;
+                this.index = this.index + 1;
+
+                this.totalMsg += msgContainer;
+                this.messageToSend = ''
+            }
+
         },
         changePerson() {
             personIsMe = personIsMe === 'end' ? 'start' : 'end';
